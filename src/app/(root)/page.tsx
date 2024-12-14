1a33a062e3b90c43403680cea6182746d9da7ff6
Parag soni <parag.soni@aspiresoftserv.com>
2024-12-14 17:20:22 +0530

    added heading in the homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-[calc(100vh-8rem)] py-4">
-      <section className="box">
-        this is the home page
+    <main className="h-[calc(100vh-8rem)] py-4">
+      <section className="box center h-full">
+        <h1>Todos Application</h1>
       </section>
     </main>
   );
